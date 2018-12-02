5047346428c9db5e020cb2308201f1f4fd6e1e31
Prateek Narang <prateeknarang111@gmail.com>
2018-12-03 02:06:38 +0530

    added MLP code

---
+++ visualize.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+def plot_decision_boundary(model, X, y,cmap=plt.cm.jet):
+    """
+    Given a model(a function) and a set of points(X), corresponding labels(y), scatter the points in X with color coding
+    according to y. Also use the model to predict the label at grid points to get the region for each label, and thus the 
+    descion boundary.
+    Example usage:
+    say we have a function predict(x,other params) which makes 0/1 prediction for point x and we want to plot
+    train set then call as:
+    plot_decision_boundary(lambda x:predict(x,other params),X_train,Y_train)
+    params(3): 
+        model : a function which expectes the point to make 0/1 label prediction
+        X : a (mx2) numpy array with the points
+        y : a (mx1) numpy array with labels
+    outputs(None)
+    """
+    # Set min and max values and give it some padding
+    x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    h = 0.01
+    # Generate a grid of points with distance h between them
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+    # Predict the function value for the whole grid
+    Z = model(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    # Plot the contour and training examples
+    plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral)
+    plt.ylabel('x2')
+    plt.xlabel('x1')
+    plt.style.use("seaborn")
+    plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.jet)
